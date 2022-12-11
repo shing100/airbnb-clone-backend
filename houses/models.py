0f27763494b9c6f6065d01b23b9bf388127feed9
lim geun <shing100@naver.com>
2022-12-12 00:33:47 +0900

    poety 사용 및 장고 설치

---
+++ houses/models.py
@@ -0,0 +1,22 @@
+from tabnanny import verbose
+from django.db import models
+
+
+class House(models.Model):
+
+    """Model Definition for Houses"""
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only", default=True
+    )
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
+
+    def __str__(self):
+        return self.name
