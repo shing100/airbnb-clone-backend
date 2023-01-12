fde58c29a7e67eccaf951df8940d5265c414d9de
lim geun <shing100@naver.com>
2023-01-12 22:10:26 +0900

    add medias, direct messages

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ CUSTOM_APPS = [
     "reviews.apps.ReviewsConfig",
     "wishlists.apps.WishlistsConfig",
     "bookings.apps.BookingsConfig",
+    "medias.apps.MediasConfig",
+    "direct_messages.apps.DirectMessagesConfig",
 ]
 
 SYSTEM_APPS = [
