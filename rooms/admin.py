332273b8ac19b183f01a6a2854eba4c7b3eabc7b
lim geun <shing100@naver.com>
2023-01-24 12:59:45 +0900

    admin action

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,14 +2,25 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
         "kind",
         "owner",
         "total_amenities",
+        "rating",
         "created_at",
     )
 
@@ -21,6 +32,12 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
     )
 
 
