968d39424c8f1669836e993ead30b6390a2654e5
lim geun <shing100@naver.com>
2023-01-25 23:06:46 +0900

    Serializer

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CategorySerializer
 from .models import Category
 
 
 @api_view()
 def categories(request):
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
     return Response(
         {
             "ok": True,
-            "categories": Category.objects.all(),
+            "categories": serializer.data,
         }
     )
