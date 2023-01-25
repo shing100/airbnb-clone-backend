8e19e386ae423e676fc1469efd441ecf3f2993ae
lim geun <shing100@naver.com>
2023-01-25 22:53:47 +0900

    django rest framework

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
@@ -49,7 +52,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
